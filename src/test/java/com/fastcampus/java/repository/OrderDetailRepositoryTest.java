345b26a3b8074db8efe697e0bc403fc8ee56fe70
chlzhfflt <chlzhfflt@gmail.com>
2021-05-28 02:10:30 +0900

    22.partner item order detail 테이블 테스트 -2

--- src/test/java/com/fastcampus/java/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/fastcampus/java/repository/OrderDetailRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class OrderDetailRepositoryTest extends JavaApplicationTests {
@@ -17,13 +18,16 @@ public class OrderDetailRepositoryTest extends JavaApplicationTests {
     public void create(){
         OrderDetail orderDetail = new OrderDetail();
 
-//        orderDetail.setOrderAt(LocalDateTime.now());
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2)); // 현재일자로부터 2일뒤쯤 도착할 것 같다
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
 
-        // 어떤 사람?
-//        orderDetail.setUserId(1L);
-
-        // 어떤 상품?
-//        orderDetail.setItemId(1L);
+        orderDetail.setOrderGroupId(1L);    // 어떠한 장바구니에
+        orderDetail.setItemId(1L);          // 어떠한 상품
+        
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
         Assertions.assertNotNull(newOrderDetail);
