19ac8fc7b973f77c29e1d99c867f051cde2c69a8
chlzhfflt <chlzhfflt@gmail.com>
2021-05-29 03:25:26 +0900

    8-3-03. 주문내역 API UPDATE

--- src/main/java/com/fastcampus/java/service/OrderGroupApiLogicService.java
+++ src/main/java/com/fastcampus/java/service/OrderGroupApiLogicService.java
@@ -51,7 +51,28 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
-        return null;
+
+        OrderGroupApiRequest body = request.getData();
+
+       return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> { // 있는경우
+                  orderGroup
+                          .setStatus(body.getStatus())
+                          .setOrderType(body.getOrderType())
+                          .setRevAddress(body.getRevAddress())
+                          .setRevName(body.getRevName())
+                          .setPaymentType(body.getPaymentType())
+                          .setTotalPrice(body.getTotalPrice())
+                          .setTotalQuantity(body.getTotalQuantity())
+                          .setOrderAt(body.getOrderAt())
+                          .setArrivalDate(body.getArrivalDate())
+                          .setUser(userRepository.getOne(body.getUserId()));
+
+                  return orderGroup;
+                })
+               .map(changeOrderGroup -> orderGroupRepository.save(changeOrderGroup))
+               .map(newOrderGroup -> response(newOrderGroup)) // (this::response)
+               .orElseGet(()->Header.ERROR("데이터 없음")); // 없는경우
     }
 
     @Override
