a1c66050191556882ace06b6dc8eba4c7ce48f34
chlzhfflt <chlzhfflt@gmail.com>
2021-05-29 01:38:41 +0900

    8-2-02. 상품 API CREATE -2

--- src/main/java/com/fastcampus/java/controller/api/ItemApiController.java
+++ src/main/java/com/fastcampus/java/controller/api/ItemApiController.java
@@ -4,33 +4,41 @@ import com.fastcampus.java.ifs.CrudInterface;
 import com.fastcampus.java.model.network.Header;
 import com.fastcampus.java.model.network.request.ItemApiRequest;
 import com.fastcampus.java.model.network.response.ItemApiResponse;
+import com.fastcampus.java.service.ItemApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/item")
 public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
 
+    @Autowired
+    private ItemApiLogicService itemApiLogicService;
+
     @Override
     @PostMapping("")    // /api/item
     public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return null;
+        return itemApiLogicService.create(request);
     }
 
     @Override
     @GetMapping("{id}") // /api/item/1 ... 1000
     public Header<ItemApiResponse> read(@PathVariable Long id) {
+
         return null;
     }
 
     @Override
     @PutMapping("")     // /api/item
     public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+
         return null;
     }
 
     @Override
     @DeleteMapping("{id}")// /api/item/1 ... 1000
     public Header delete(@PathVariable Long id) {
+
         return null;
     }
 }
