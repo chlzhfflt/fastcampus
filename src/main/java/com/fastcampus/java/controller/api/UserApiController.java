d5907266e6640f048d35b72a3856e9e3ea0b83ba
chlzhfflt <chlzhfflt@gmail.com>
2021-05-28 19:47:16 +0900

    8-1-02. 사용자 API CREATE -2

--- src/main/java/com/fastcampus/java/controller/api/UserApiController.java
+++ src/main/java/com/fastcampus/java/controller/api/UserApiController.java
@@ -4,17 +4,24 @@ import com.fastcampus.java.ifs.CrudInterface;
 import com.fastcampus.java.model.network.Header;
 import com.fastcampus.java.model.network.request.UserApiRequest;
 import com.fastcampus.java.model.network.response.UserApiResponse;
+import com.fastcampus.java.service.UserApiLogicService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
     @Override
     @PostMapping("")    // /api/user
-    public Header<UserApiResponse> create(@RequestBody UserApiRequest userApiRequest) {
-
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        log.info("{}",request);
+        return userApiLogicService.create(request);
     }
 
     @Override
@@ -26,7 +33,7 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
 
     @Override
     @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody UserApiRequest request) {
+    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
 
         return null;
     }
--- src/main/java/com/fastcampus/java/ifs/CrudInterface.java
+++ src/main/java/com/fastcampus/java/ifs/CrudInterface.java
@@ -5,11 +5,11 @@ import com.fastcampus.java.model.network.request.UserApiRequest;
 
 public interface CrudInterface<Req,Res> {
 
-    Header<Res> create(Req request);    // todo request object 추가
+    Header<Res> create(Header<Req> request);    // todo request object 추가
 
     Header<Res> read(Long id);
 
-    Header<Res> update(Req request);
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 }
