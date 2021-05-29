1717a1b3a07c624e7a1e13be0cc7eeb9a870521c
chlzhfflt <chlzhfflt@gmail.com>
2021-05-29 21:47:56 +0900

    9-06. 추상화 코드 적용해보기

--- src/main/java/com/fastcampus/java/model/entity/Partner.java
+++ src/main/java/com/fastcampus/java/model/entity/Partner.java
@@ -1,5 +1,6 @@
 package com.fastcampus.java.model.entity;
 
+import com.fastcampus.java.model.enumclass.PartnerStatus;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -28,7 +29,8 @@ public class Partner {
 
     private String name;
 
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private PartnerStatus status;
 
     private String address;
 
