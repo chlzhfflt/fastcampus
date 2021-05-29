8e709f6055e0df727940eaeb122b10a43aa2c5f2
chlzhfflt <chlzhfflt@gmail.com>
2021-05-29 15:36:54 +0900

    9-01. JPA Enum 형태의 값 관리 -1

--- src/main/java/com/fastcampus/java/model/entity/User.java
+++ src/main/java/com/fastcampus/java/model/entity/User.java
@@ -1,5 +1,6 @@
 package com.fastcampus.java.model.entity;
 
+import com.fastcampus.java.model.enumclass.UserStatus;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -31,7 +32,8 @@ public class User {
 
     private String password;
 
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private UserStatus status; // REGISTERED , UNREGISTERED , WAITING.. 고정된 값을 사용함
 
     private String email;
 
