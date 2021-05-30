f6d8523305e64c26d1ae5861390c6312788d92da
chlzhfflt <chlzhfflt@gmail.com>
2021-05-30 21:35:56 +0900

    어드민 페이지 마무리

--- src/main/java/com/fastcampus/java/service/SettlementApiLogicService.java
+++ src/main/java/com/fastcampus/java/service/SettlementApiLogicService.java
@@ -13,18 +13,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class SettlementApiLogicService extends BaseService<SettlementApiRequest, SettlementApiResponse, Settlement> {
 
+//    CREATE TABLE `settlement` (
+//            `user_id` bigint NOT NULL,
+//            `total_price` decimal(12,4) NOT NULL,
+//  `created_at` datetime NOT NULL,
+//            `created_by` varchar(20) NOT NULL,
+//  `updated_at` datetime DEFAULT NULL,
+//            `updated_by` varchar(20) DEFAULT NULL,
+//    PRIMARY KEY (`user_id`)
+//) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+    // orderGroup 테이블에 있는 userId와 totalPrice를 가지고와서 저장후 read를 하면 되겠다고 간단히 생각했는데
+    // 작성하다보니 userId가 아닌 userId와 totalPrice를 보유한 settlement의 id값으로 get된다는걸 깨달았습니다
+    // 프라이멀키 id값을 테이블에서 지우고 조금씩 고쳐보다가 코드가 계속 꼬여서 겨우 복구만하고 마무리합니다..
+    // 첫단추부터 잘못꿴거같아서 나중에 시간을 두고 곰곰히 생각해봐야할 것 같습니다..
+
     @Autowired
     private OrderGroupRepository orderGroupRepository;
 
     @Override
     public Header<SettlementApiResponse> create(Header<SettlementApiRequest> request) {
 
-        SettlementApiRequest body = request.getData();
-
-        Settlement settlement = Settlement.builder()
-                .orderGroup(orderGroupRepository.getOne(body.getUserId()))
-                .orderGroup(orderGroupRepository.getOne(body.getTotalPrice()))
-                .build();
+//        SettlementApiRequest body = request.getData();
+//
+//        Settlement settlement = Settlement.builder()
+//                .orderGroup(orderGroupRepository.getOne(body.getUserId()))
+//                .orderGroup(orderGroupRepository.getOne(body.getTotalPrice()))
+//                .build();
 
         return null;
     }
