9252d3b2e1fe79349db4014632f1cf8d913c3efc
chlzhfflt <chlzhfflt@gmail.com>
2021-06-11 03:02:47 +0900

    Chapter 03. JPA - 04. Lombok (2) - 2

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/Person.java
@@ -8,12 +8,10 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
 public class Person {
 
     @Id
@@ -28,6 +26,7 @@ public class Person {
 
     private String hobby;
 
+    @NonNull
     private String bloodType;
 
     private String address;
@@ -38,21 +37,4 @@ public class Person {
 
     @ToString.Exclude
     private String phoneNumber;
-
-    public boolean equals(Object object){
-        if(object == null){
-            return false;
-        }
-        Person person = (Person) object;
-
-        if(!person.getName().equals(this.getName())){
-            return false;
-        }
-
-        if(person.getAge() != this.getAge()){
-            return false;
-        }
-
-        return true;
-    }
 }
