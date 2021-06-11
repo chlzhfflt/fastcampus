b75d29a39d0f2eb485c940523ab5c3662e44c9d9
chlzhfflt <chlzhfflt@gmail.com>
2021-06-12 02:11:20 +0900

    Chapter 04. 05. JPA @Query / build.gradle 추가

---
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/domain/dto/Birthday.java
@@ -0,0 +1,31 @@
+package com.fastcampus.javaallinone.project3.mycontact.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.time.LocalDate;
+
+@Embeddable // entity에 속해있는 dto
+@Data
+@NoArgsConstructor
+public class Birthday {
+    private int yearOfBirthday;
+
+    @Min(1)
+    @Max(12)
+    private int monthOfBirthday;
+
+    @Min(1)
+    @Max(31)
+    private int dayOfBirthday;
+
+    public Birthday(LocalDate birthday){ // birthday 를 localdate타입으로 한번 랩핑해줌
+        this.yearOfBirthday = birthday.getYear();
+        this.monthOfBirthday = birthday.getMonthValue();
+        this.dayOfBirthday = birthday.getDayOfMonth();
+    }
+}
--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.javaallinone.project3.mycontact.repository;
 
 import com.fastcampus.javaallinone.project3.mycontact.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,5 +15,10 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByBloodType(String bloodType);
 
-    List<Person> findByBirthdayBetween(LocalDate startDate, LocalDate endDate); // 날짜값 두개를 받아서 생일을 구해오는 값
+//    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = ?1 and person.birthday.dayOfBirthday = ?2") // JPQL - entity 기반 query , ?1 은 첫번째 인자를 의미함
+//    List<Person> findByMonthOfBirthday(int monthOfBirthday, int dayOfBirthday); // 8월이 생일인 사람을 구해옴
+//    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday and person.birthday.dayOfBirthday = :dayOfBirthday") // 이름 기반
+//    @Query(value = "select * from person where month_of_birthday = :monthOfBirthday and day_of_birthday = :dayOfBirthday", nativeQuery = true)
+    @Query(value = "select person from Person person where person.birthday.monthOfBirthday = :monthOfBirthday")
+    List<Person> findByMonthOfBirthday(@Param("monthOfBirthday") int monthOfBirthday); // 이름 기반
 }
