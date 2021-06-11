f4d7f88c0e0f0aad426699dfa7a8ac905fb224d8
chlzhfflt <chlzhfflt@gmail.com>
2021-06-11 23:04:37 +0900

    Chapter 04. 02. JPA Relation - 2

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -14,15 +14,10 @@ import java.util.stream.Collectors;
 public class PersonService {
     @Autowired
     private PersonRepository personRepository;
-    @Autowired
-    private BlockRepository blockRepository;
     
     public List<Person> getPeopleExcludeBlocks(){ // 차단된 사람을 제외하고 나머지 전체 사람을 가져오는 로직
         List<Person> people = personRepository.findAll();
-        List<Block> blocks = blockRepository.findAll();
-        List<String> blockNames = blocks.stream().map(Block::getName).collect(Collectors.toList()); // blocks에 들어있는 모든 값을 순회하면서 getName만 뽑아옴
         
-        return people.stream().filter(person -> !blockNames.contains(person.getName())).collect(Collectors.toList()); // filter - 어떤 조건에 일치하는 것만 돌려주는 함수
-                                                                                                                      // blockNames에 person에 getName이 들어있다면 제외하고싶음
+        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList()); // block data가 없는 사람만 가져옴
     }
 }
