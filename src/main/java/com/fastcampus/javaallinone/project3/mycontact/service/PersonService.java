882357777457b35cc69f0198a73ce3e3dc35254b
chlzhfflt <chlzhfflt@gmail.com>
2021-06-13 15:56:19 +0900

    Chapter 04. Controller - 07. @GetMapping 사용하기

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,7 +29,8 @@ public class PersonService {
 
     @Transactional(readOnly = true)
     public Person getPerson(Long id){
-        Person person = personRepository.findById(id).get();
+//        Person person = personRepository.findById(id).get();
+        Person person = personRepository.findById(id).orElse(null); // get을 하는데 값이 없으면 null을 return한다는 의미
 
 //        System.out.println("person : " + person);
         log.info("person : {}", person);
